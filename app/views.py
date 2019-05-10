6f80aebdf880a3a87123f8a9c639e997baf14671
chingyi-lin <icequ60914@gmail.com>
2019-05-10 11:44:16 -0700

    rearrange code blocks for readability

--- app/views.py
+++ app/views.py
@@ -57,6 +57,177 @@ def login():
     resp = Response(json.dumps(data), status=status_code, mimetype='application/json')
     return resp
 
+@app.route('/visit', methods=['POST'])
+def send_visit_intention():
+    args = request.get_json()
+    user_id = int(args.get('userId'))
+    pet_id = int(args.get('petId'))
+    activity = args.get('activityType')
+    date_str = args.get("visitDate")
+    format_date = '%m/%d/%Y'
+    time_slot = datetime.strptime(date_str, format_date)
+    if not isValidUserId(user_id):
+        message =  {"Error":"There is no user at that id"}
+        status_code = 404
+    elif not isValidAnimalId(pet_id):
+        message =  {"Error":"There is no pet at that id"}
+        status_code = 404
+    elif not is_valid_activity(activity.lower()):
+        message = {"Error":"No such activity"}
+        status_code = 404
+    else:
+        new_intention = Intention(activity,time_slot, pet_id,user_id)
+        addToDatabase(new_intention)
+        message = ""
+        status_code = 200
+    return Response(json.dumps(message), status= status_code, mimetype='application/json')
+
+def is_valid_activity(activity):
+    return activity in ['volunteering', 'visiting', 'both']
+
+@app.route('/load-animal', methods=['POST'])
+def load_dogs():
+    def get_dogs():
+        data = {
+            "ZipCode": "94703",
+            "SearchRadiusInMiles": 50,
+            "PetType": "dog",
+            "PageNumber": 1
+        }
+        response = requests.post("https://getyourpet.com/api/partnerpetsearch", json=data)
+        return response.json()
+    data = get_dogs()
+    for dog in data:
+        name = dog['Name']
+        if dog['Breeds'] != None:
+            breed = ''
+            for b in dog['Breeds']:
+                breed += b['BreedName']
+                breed += ' & '
+            breed = breed[:-3]
+        else:
+            breed = 'N/A'
+        picture_url = dog['PrimaryPhotoUrl']
+        gender = dog['Gender']
+        age = dog['AgeYears']
+        availability = dog['NewlyAvailable']
+        activity = random.choice(['Just had a walk! :)', 'Walking with a visitor!', 'Sleeping', 'Feeling bored...'])
+        addToDatabase(Animal(name, breed, picture_url, gender, age, availability, activity))
+
+    return Response(json.dumps(data), status= 200, mimetype='application/json')
+
+@app.route('/animal', methods=['POST'])
+def add_animal_info():
+    args = request.get_json()
+    pic_url = args.get('picture_url')
+    name = args.get('name')
+    age = args.get('age')
+    breed = args.get('breed')
+    gender = args.get('gender')
+    availability = args.get('availability')
+    if pic_url is None or name is None or age is None or breed is None or gender is None or availability is None:
+        status = 400
+        response = {"Error": "Arguments missing"}
+    else:
+        status = 200
+        new_pet = Animal(name, breed, pic_url, gender, age, availability, id=None)
+        addToDatabase(new_pet)
+        response = {"id": new_pet.id}
+    return Response(json.dumps(response), status= status, mimetype='application/json')
+
+@app.route('/animal/<id>', methods=['GET'])
+def get_dog(id):
+    dog = Animal.query.filter_by(id=id).first()
+    if dog is None:
+        status = 404
+        response = {"message": f"Animal('id': {id}) does not exist"}
+    else:
+        data = {
+            'name': dog.name,
+            'breed': dog.breed,
+            'picture_url': dog.picture_url,
+            'gender': dog.gender,
+            'age': dog.age,
+            'availability': dog.availability
+        }
+        status = 200
+        response = data
+    return Response(json.dumps(response), status= status, mimetype='application/json')
+
+@app.route('/animal/<id>', methods=['PUT'])
+def update_dog(id):
+    dog = Animal.query.filter_by(id=id).first()
+    if dog is None:
+        status = 404
+        response = {"message": f"Animal('id': {id}) does not exist"}
+    else:
+        args = request.get_json()
+        status = 200
+        if 'name' in args:
+            dog.name = args['name']
+        if 'breed' in args:
+            dog.breed = args['breed']
+        if 'picture_url' in args:
+            dog.picture_url = args['picture_url']
+        if 'gender' in args:
+            dog.gender = args['gender']
+        if  'age' in args:
+            dog.age = args['age']
+        if 'availability' in args:
+            dog.availability = args['availability']
+        db.session.commit()
+        response = {"message": str(dog)}
+    return Response(json.dumps(response), status= status, mimetype='application/json')
+
+@app.route('/animal/activity/<pet_id>', methods=['GET'])
+def get_animal_activity(pet_id):
+    pet_id = int(pet_id)
+
+    if not isValidAnimalId(pet_id):
+        response =  {"Error":"There is no pet at that id"}
+        status_code = 404
+
+    else:
+        get_activity = getAnimalByPetId(pet_id).activity
+        response = {"activity": get_activity}
+
+    return Response(json.dumps(response), status= 200, mimetype='application/json')
+
+
+
+@app.route('/animal/availability/<pet_id>', methods=['GET'])
+def get_animal_availability(pet_id):
+    pet_id = int(pet_id)
+
+    if not isValidAnimalId(pet_id):
+        response =  {"Error":"There is no pet at that id"}
+        status_code = 404
+
+    else:
+        get_availability = getAnimalByPetId(pet_id).availability
+        response = {"availability": get_availability}
+
+    return Response(json.dumps(response), status= 200, mimetype='application/json')
+
+@app.route('/donate', methods=['POST'])
+def add_donation_info():
+    args = request.get_json()
+    user_id = int(args.get('userId'))
+    pet_id = int(args.get('petId'))
+    amount = float(args.get('amount'))
+    if not isValidUserId(user_id):
+        message =  {"Error":"There is no user at that id"}
+        status_code = 404
+    elif not isValidAnimalId(pet_id):
+        message =  {"Error":"There is no pet at that id"}
+        status_code = 404
+    else:
+        new_donation = Donation(amount, user_id, pet_id)
+        addToDatabase(new_donation)
+        message = ""
+        status_code = 200
+    return Response(json.dumps(message), status= status_code, mimetype='application/json')
+
 # --- Page rendering methods: --- #
 # It's just for UI. We can remove it later.
 
@@ -116,175 +287,3 @@ def logout():
 def unauthorized_handler():
     return redirect(url_for('login'))
 
-@app.route('/visit', methods=['POST'])
-def send_visit_intention():
-    args = request.get_json()
-    user_id = int(args.get('userId'))
-    pet_id = int(args.get('petId'))
-    activity = args.get('activityType')
-    date_str = args.get("visitDate")
-    format_date = '%m/%d/%Y'
-    time_slot = datetime.strptime(date_str, format_date)
-    if not isValidUserId(user_id):
-        message =  {"Error":"There is no user at that id"}
-        status_code = 404
-    elif not isValidAnimalId(pet_id):
-        message =  {"Error":"There is no pet at that id"}
-        status_code = 404
-    elif not is_valid_activity(activity.lower()):
-        message = {"Error":"No such activity"}
-        status_code = 404
-    else:
-        new_intention = Intention(activity,time_slot, pet_id,user_id)
-        addToDatabase(new_intention)
-        message = ""
-        status_code = 200
-    return Response(json.dumps(message), status= status_code, mimetype='application/json')
-
-def is_valid_activity(activity):
-    return activity in ['volunteering', 'visiting', 'both']
-
-@app.route('/animal', methods=['POST'])
-def add_animal_info():
-    args = request.get_json()
-    pic_url = args.get('picture_url')
-    name = args.get('name')
-    age = args.get('age')
-    breed = args.get('breed')
-    gender = args.get('gender')
-    availability = args.get('availability')
-    if pic_url is None or name is None or age is None or breed is None or gender is None or availability is None:
-        status = 400
-        response = {"Error": "Arguments missing"}
-    else:
-        status = 200
-        new_pet = Animal(name, breed, pic_url, gender, age, availability, id=None)
-        addToDatabase(new_pet)
-        response = {"id": new_pet.id}
-    return Response(json.dumps(response), status= status, mimetype='application/json')
-
-
-@app.route('/animal/availability/<pet_id>', methods=['GET'])
-def get_animal_availability(pet_id):
-    pet_id = int(pet_id)
-
-    if not isValidAnimalId(pet_id):
-        response =  {"Error":"There is no pet at that id"}
-        status_code = 404
-
-    else:
-        get_availability = getAnimalByPetId(pet_id).availability
-        response = {"availability": get_availability}
-
-    return Response(json.dumps(response), status= 200, mimetype='application/json')
-
-
-@app.route('/animal/activity/<pet_id>', methods=['GET'])
-def get_animal_activity(pet_id):
-    pet_id = int(pet_id)
-
-    if not isValidAnimalId(pet_id):
-        response =  {"Error":"There is no pet at that id"}
-        status_code = 404
-
-    else:
-        get_activity = getAnimalByPetId(pet_id).activity
-        response = {"activity": get_activity}
-
-    return Response(json.dumps(response), status= 200, mimetype='application/json')
-
-
-@app.route('/load-animal', methods=['POST'])
-def load_dogs():
-    def get_dogs():
-        data = {
-            "ZipCode": "94703",
-            "SearchRadiusInMiles": 50,
-            "PetType": "dog",
-            "PageNumber": 1
-        }
-        response = requests.post("https://getyourpet.com/api/partnerpetsearch", json=data)
-        return response.json()
-    data = get_dogs()
-    for dog in data:
-        name = dog['Name']
-        if dog['Breeds'] != None:
-            breed = ''
-            for b in dog['Breeds']:
-                breed += b['BreedName']
-                breed += ' & '
-            breed = breed[:-3]
-        else:
-            breed = 'N/A'
-        picture_url = dog['PrimaryPhotoUrl']
-        gender = dog['Gender']
-        age = dog['AgeYears']
-        availability = dog['NewlyAvailable']
-        activity = random.choice(['Just had a walk! :)', 'Walking with a visitor!', 'Sleeping', 'Feeling bored...'])
-        addToDatabase(Animal(name, breed, picture_url, gender, age, availability, activity))
-
-    return Response(json.dumps(data), status= 200, mimetype='application/json')
-
-
-@app.route('/animal/<id>', methods=['GET'])
-def get_dog(id):
-    dog = Animal.query.filter_by(id=id).first()
-    if dog is None:
-        status = 404
-        response = {"message": f"Animal('id': {id}) does not exist"}
-    else:
-        data = {
-            'name': dog.name,
-            'breed': dog.breed,
-            'picture_url': dog.picture_url,
-            'gender': dog.gender,
-            'age': dog.age,
-            'availability': dog.availability
-        }
-        status = 200
-        response = data
-    return Response(json.dumps(response), status= status, mimetype='application/json')
-
-@app.route('/animal/<id>', methods=['PUT'])
-def update_dog(id):
-    dog = Animal.query.filter_by(id=id).first()
-    if dog is None:
-        status = 404
-        response = {"message": f"Animal('id': {id}) does not exist"}
-    else:
-        args = request.get_json()
-        status = 200
-        if 'name' in args:
-            dog.name = args['name']
-        if 'breed' in args:
-            dog.breed = args['breed']
-        if 'picture_url' in args:
-            dog.picture_url = args['picture_url']
-        if 'gender' in args:
-            dog.gender = args['gender']
-        if  'age' in args:
-            dog.age = args['age']
-        if 'availability' in args:
-            dog.availability = args['availability']
-        db.session.commit()
-        response = {"message": str(dog)}
-    return Response(json.dumps(response), status= status, mimetype='application/json')
-
-@app.route('/donate', methods=['POST'])
-def add_donation_info():
-    args = request.get_json()
-    user_id = int(args.get('userId'))
-    pet_id = int(args.get('petId'))
-    amount = float(args.get('amount'))
-    if not isValidUserId(user_id):
-        message =  {"Error":"There is no user at that id"}
-        status_code = 404
-    elif not isValidAnimalId(pet_id):
-        message =  {"Error":"There is no pet at that id"}
-        status_code = 404
-    else:
-        new_donation = Donation(amount, user_id, pet_id)
-        addToDatabase(new_donation)
-        message = ""
-        status_code = 200
-    return Response(json.dumps(message), status= status_code, mimetype='application/json')
